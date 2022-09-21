acbeeda961065e750ec914d030ba73404fa8dc5c
Warner-Felicia <foodleswarner@gmail.com>
2022-09-21 10:21:12 -0400

    completed course project - the basics

---
+++ recipe-app/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    templateUrl: './shopping-edit.component.html',
+    styleUrls: ['./shopping-edit.component.css'],
+    selector: 'app-shopping-edit'
+})
+export class ShoppingEditComponent {
+    
+}
---
+++ recipe-app/src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+import { Ingredient } from '../shared/ingredient.model';
+
+@Component({
+    templateUrl: './shopping-list-component.html',
+    styleUrls: ['./shopping-list.component.css'],
+    selector: 'app-shopping-list'
+
+})
+
+export class ShoppingListComponent {
+    ingredients: Ingredient[] = [
+        new Ingredient('Apples', 5),
+        new Ingredient('Tomatoes', 10)
+    ];
+
+}
