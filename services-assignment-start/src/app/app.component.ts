8f44907ee00a1cc2367914647b53bb8c320314fa
Warner-Felicia <foodleswarner@gmail.com>
2022-10-12 20:54:48 -0400

    completed services assignment

--- services-assignment-start/src/app/app.component.ts
+++ services-assignment-start/src/app/app.component.ts
@@ -1,21 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { CounterService } from './counter.service';
+import { UsersService } from './users.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [UsersService]
 })
-export class AppComponent {
-  activeUsers = ['Max', 'Anna'];
-  inactiveUsers = ['Chris', 'Manu'];
+export class AppComponent implements OnInit{
+  activeToInactiveCounter: number;
+  inactivetoActiveCounter: number;
 
-  onSetToInactive(id: number) {
-    this.inactiveUsers.push(this.activeUsers[id]);
-    this.activeUsers.splice(id, 1);
-  }
+  constructor(private counterService: CounterService) { }
 
-  onSetToActive(id: number) {
-    this.activeUsers.push(this.inactiveUsers[id]);
-    this.inactiveUsers.splice(id, 1);
+  ngOnInit() {
+    this.activeToInactiveCounter = this.counterService.activeToInactiveCounter;
+    this.inactivetoActiveCounter = this.counterService.inactiveToActiveCounter;
+
   }
 }
